384321fea4d3f827f1c29b66fc126be3f682cc16
Pradeep4157 <krp18539@gmail.com>
2026-03-02 20:00:38 +0530

    added products to the page, yet to add the images..

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Products from "./components/Products/Products";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Products />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,3 +1,19 @@
+import productsData from "../../data/products.json";
+import "../Products/Products.css";
 function Products() {
-  return <div className="products"></div>;
+  return (
+    <div className="products-container">
+      {productsData.map((product) => (
+        // this key is mostly going to get used to add this ele
+        // to cart..
+        <div key={product.id} className="product">
+          {/* <img src={product.image}></img> */}
+          <div className="product-name">{product.name}</div>
+          <button>Add to Cart</button>
+        </div>
+      ))}
+    </div>
+  );
 }
+
+export default Products;
