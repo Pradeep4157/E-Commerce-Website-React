919c512ee1c777002217583e0d6f07a6f485b49b
Pradeep4157 <krp18539@gmail.com>
2026-03-04 19:53:26 +0530

    partially done with general approach to functionalities...

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,53 @@
+/*
+  now we need to plan how are we going to 
+
+  send these things in modal.. 
+
+  because its not simple children props that we 
+
+  send there and just store it at their places.. 
+
+  at least that's what he said..
+
+  what info do i have ?
+
+  onClick with this onClick what i think is that we 
+
+  are going to pass what ?
+
+  in general what does it need to render that div.. 
+
+  it needs the function 
+
+  bhat all R bhe going to send in Modal comp ?
+
+  close function.. 
+
+  true or false.. 
+
+
+
+*/
+
 import { useState } from "react";
 import "./Header.css";
-import "../../UI/Modal.css";
-
-function onAddProductClick() {}
+import "../UI/Modal.css";
+import Modal from "../UI/Modal";
 
 function Header() {
   const [showCart, setShowCart] = useState(false);
+  const [freqProduct, setAddProduct] = useState(false);
   function openCart() {
     setShowCart(true);
   }
   function closeCart() {
     setShowCart(false);
+  }
+  function openAddProduct() {
+    setAddProduct(true);
+  }
+  function closeAddProduct() {
+    setAddProduct(false);
   }
 
   return (
@@ -19,7 +56,7 @@ function Header() {
       <div>
         <button
           className="yellow-button"
-          onClick={onAddProductClick}
+          onClick={openAddProduct}
           style={{ marginRight: "20px" }}
         >
           Add Product
@@ -28,13 +65,11 @@ function Header() {
           Cart
         </button>
       </div>
-      <div>
-        {showCart && (
-          <div className="backdrop" onClick={closeCart}>
-            <div className="modal">LUND LELE !!</div>
-          </div>
-        )}
-      </div>
+      <Modal
+        show={showCart}
+        onClose={closeCart}
+        children={<div>LUND LELE !! </div>}
+      />
     </header>
   );
 }
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,11 @@
+function Modal({ show, onClose, children }) {
+  if (show === false) {
+    return null;
+  }
+  return (
+    <div className="backdrop" onClick={onClose}>
+      <div className="modal">{children}</div>
+    </div>
+  );
+}
+export default Modal;
