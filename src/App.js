9c190c2156dac9f227456c3a7d9bc4d070d141f6
Pradeep4157 <krp18539@gmail.com>
2026-03-15 15:46:56 +0530

    added some more functionalities

--- src/App.js
+++ src/App.js
@@ -22,6 +22,12 @@ function App() {
   const openCart = () => setShowCart(true);
   const closeCart = () => setShowCart(false);
   const [cartItems, setCartItems] = useState([]);
+  const handleIncreaseQuantity = () => {
+    console.log("Quantity Increased..");
+  };
+  const handleDecreaseQuantity = () => {
+    console.log("Quantity Decreased..");
+  };
   const handleAddToCart = (productId, productName, productImage) => {
     // let updatedCartItems = cartItems;
     // updatedCartItems = updatedCartItems.concat({
@@ -32,13 +38,23 @@ function App() {
     // });
     // instead of all this we can use spread operator to spread
     // the array and just append at the end of the array..
-    const cartItem = {
-      id: productId,
-      name: productName,
-      image: productImage,
-      quantity: 1,
-    };
-    setCartItems((cartItems) => [...cartItems, cartItem]);
+    const productInCartIndex = cartItems.findIndex(
+      (item) => item.id === productId,
+    );
+    if (productInCartIndex === -1) {
+      const cartItem = {
+        id: productId,
+        name: productName,
+        image: productImage,
+        quantity: 1,
+      };
+      setCartItems((cartItems) => [...cartItems, cartItem]);
+    } else {
+      // inc the value in the cart..
+      var updatedCartItems = [...cartItems];
+      updatedCartItems[productInCartIndex].quantity += 1;
+    }
+
     // setCartItems(updatedCartItems);
   };
   return (
@@ -49,6 +65,8 @@ function App() {
         showCart={showCart}
         closeCart={closeCart}
         cartItems={cartItems}
+        onIncreaseQuantity={handleIncreaseQuantity}
+        onDecreaseQuantity={handleDecreaseQuantity}
       ></Cart>
     </div>
   );
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,13 @@
 import Modal from "../UI/Modal";
 import "./Cart.css";
-function CartItem({ id, name, image, quantity }) {
+function CartItem({
+  id,
+  name,
+  image,
+  quantity,
+  onIncreaseQuantity,
+  onDecreaseQuantity,
+}) {
   return (
     <div className="cart-item">
       <div className="item-img">
@@ -11,12 +18,18 @@ function CartItem({ id, name, image, quantity }) {
         <div className="item-qty">
           <div>Qty : {quantity}</div>
           <div>
-            <button className="yellow-button qty-button qty-plus-button">
+            <button
+              className="yellow-button qty-button qty-plus-button"
+              onClick={onIncreaseQuantity}
+            >
               +
             </button>
           </div>
           <div>
-            <button className="yellow-button qty-button qty-minus-button">
+            <button
+              className="yellow-button qty-button qty-minus-button"
+              onClick={onDecreaseQuantity}
+            >
               -
             </button>
           </div>
@@ -25,19 +38,41 @@ function CartItem({ id, name, image, quantity }) {
     </div>
   );
 }
-function Cart({ showCart, closeCart, cartItems }) {
+function Cart({
+  showCart,
+  closeCart,
+  cartItems,
+  onIncreaseQuantity,
+  onDecreaseQuantity,
+}) {
   return (
     <Modal show={showCart} onClose={closeCart}>
-      <div>
-        {cartItems.map((item) => (
-          <CartItem
-            key={item.id}
-            id={item.id}
-            name={item.name}
-            image={item.image}
-            quantity={item.quantity}
-          ></CartItem>
-        ))}
+      <div className="cart-container">
+        <div className="cart-heading">Cart</div>
+        {cartItems.length > 0 ? (
+          cartItems.map((item) => (
+            <CartItem
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              quantity={item.quantity}
+              onIncreaseQuantity={onIncreaseQuantity}
+              onDecreaseQuantity={onDecreaseQuantity}
+            ></CartItem>
+          ))
+        ) : (
+          <div className="empty-cart">Cart is Empty</div>
+        )}
+        <div className="cart-buttons">
+          <button className="black-button close-cart" onClick={closeCart}>
+            Close
+          </button>
+
+          {cartItems.length > 0 && (
+            <button className="yellow-button">CheckOut</button>
+          )}
+        </div>
       </div>
     </Modal>
   );
